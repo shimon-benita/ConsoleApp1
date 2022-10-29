7b3cede7ab01346127c9e51b398e9be632a00b90
shimon-benita <shimonbenita87@gmail.com>
2022-10-29 17:29:48 +0300

    Add project files.

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,10 @@
+﻿public class Person
+{
+    public string? firstName;
+    public string? lastName;
+
+    public void Introduce()
+    {
+        Console.WriteLine("Hello! my name is: " + firstName + " " + lastName);
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp1.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var menash = new Person
+            {
+                firstName = "Menash",
+                lastName = "Ben Ita"
+            };
+
+            menash.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+                
+
+        }
+
+    }
+}
